e7702b2109a3024cf0d70d4dc3778cc4501f67a9
Pixeladed <vietminhle98@gmail.com>
2021-01-22 14:54:52 +0800

    feat(artifact): add copyTo method for artifact

--- src/artifact/index.ts
+++ src/artifact/index.ts
@@ -1,5 +1,7 @@
 import { ArtifactManifest } from '../network';
 import { LocalLayer } from './types';
+import path from 'path';
+import fs from 'fs';
 
 export default class Artifact {
   name: string;
@@ -17,5 +19,28 @@ export default class Artifact {
     this.reference = content.reference;
     this.manifest = content.manifest;
     this.layers = content.layers;
+  }
+
+  async copyTo(destinationPath: string) {
+    const absolutePath = path.resolve(destinationPath);
+    if (
+      !fs.existsSync(absolutePath) ||
+      !fs.statSync(absolutePath).isDirectory()
+    ) {
+      throw new Error('The destination does not exist or is not a directory');
+    }
+
+    const ops = this.layers.map(
+      layer =>
+        new Promise((resolve, reject) => {
+          const filename = path.parse(layer.path).base;
+          const destinationName = path.resolve(absolutePath, filename);
+          fs.copyFile(layer.path, destinationName, reject);
+
+          return resolve(destinationName);
+        })
+    );
+
+    await Promise.all(ops);
   }
 }
